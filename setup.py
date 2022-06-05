c9c09045f2e9ce769b3128bbb1e41d8fbdc3f109
Swadeesh <swadeesh@gmail.com>
2022-06-05 17:58:57 -0400

    Package name changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='passwordgenerator',
+    name='pypassword-generator',
     version='0.1.0',
     author='Swadeesh',
     author_email='swadeesh@gmail.com',
